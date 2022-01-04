70f08730156a0baadf374d695677a3aaf0f4995a
Angel <angel.hernandez.12>
2022-01-04 04:22:01 -0400

    Added Vue & Quasar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,12 @@
 export default {
+    template: `
+    <q-layout class="window-width window-height items-center justify-center">
+    <q-page>
+    <span class="text-h6">My First Vue 3 + Quasar Github Website</span>
+    <q-btn color="primary" label="Click Me"></q-btn>
+    </q-page>
+    </q-layout>
+    `,
     setup() {
         
     }
