a3186b647dcea9ca1ff1f457fcd7f581650f2b60
Angel <angel.hernandez.12>
2022-01-04 19:44:07 -0400

    Changed warning colors To Red, added warning icon

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -10,8 +10,8 @@ export default {
     </q-header>
     <q-page-container class="window-width window-height column q-gutter-md items-center justify-center">
     <span class="text-h6">Welcome to Angel Hdz Multimedia.</span>
-    <span class="text-h4">Site under construction.</span>
-    <q-btn @click="$q.notify({message: 'Click'})" color="negative" label="Click Me"></q-btn>
+    <span class="text-h4 text-negative"><q-icon name="warning"></q-icon>Website Under Construction!</span>
+    <q-btn @click="$q.notify({message: 'Click'})" color="primary" label="Click Me"></q-btn>
     </q-page-container>
     </q-layout>
     `,
