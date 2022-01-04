4589a31a903683d35f79bb6e655075ea720f57b2
Angel <angel.hernandez.12>
2022-01-04 04:30:24 -0400

    Added ,js To Vue HomePage

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@3/dist/vue.global.prod.js'
 import Quasar from 'https://cdn.jsdelivr.net/npm/quasar@2/dist/quasar.umd.prod.js'
-import HomePage from './pages/HomePage'
+import HomePage from './pages/HomePage.js'
 
 
 const app = Vue.createApp(HomePage)
