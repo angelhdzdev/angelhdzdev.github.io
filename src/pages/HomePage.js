ed91f290074cd0f157fced3144f9eed1293bca9a
Angel <angel.hernandez.12>
2022-01-04 18:20:34 -0400

    Added Page Welcome Text

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,12 +4,13 @@ export default {
     <q-header>
         <q-toolbar>
         <q-toolbar-title>
-        Home 
+        Angel Hdz Dev - Home 
         </q-toolbar-title>
         </q-toolbar>
     </q-header>
     <q-page-container class="window-width window-height column items-center justify-center">
-    <span class="text-h6">My First Vue 3 + Quasar Github Website</span>
+    <span class="text-h6">Welcome to Angel Hdz Dev.</span>
+    <span class="text-h4">Site under construction.</span>
     <q-btn @click="$q.notify({message: 'Click'})" color="negative" label="Click Me"></q-btn>
     </q-page-container>
     </q-layout>
