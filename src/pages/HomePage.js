df38906f174827d0276b9da06982e7a1dafba0e8
Angel <angel.hernandez.12>
2022-01-04 19:28:45 -0400

    Changed Github Username, Updated Webpage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,12 +4,12 @@ export default {
     <q-header>
         <q-toolbar>
         <q-toolbar-title>
-        Home - Angel Hdz Dev
+        Home - Angel Hdz Multimedia
         </q-toolbar-title>
         </q-toolbar> 
     </q-header>
     <q-page-container class="window-width window-height column items-center justify-center">
-    <span class="text-h6">Welcome to Angel Hdz Dev.</span>
+    <span class="text-h6">Welcome to Angel Hdz Multimedia.</span>
     <span class="text-h4">Site under construction.</span>
     <q-btn @click="$q.notify({message: 'Click'})" color="negative" label="Click Me"></q-btn>
     </q-page-container>
