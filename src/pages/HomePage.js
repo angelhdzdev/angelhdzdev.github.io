503b8c3ac104ffcf71df4e2999883726780ecd71
Angel <angel.hernandez.12>
2022-01-04 05:08:50 -0400

    Fixed imports, added QToolbar, QButton and Click Event

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,22 @@
 export default {
     template: `
-    <q-layout class="window-width window-height items-center justify-center">
-    <q-page>
+    <q-layout >
+    <q-header>
+        <q-toolbar>
+        <q-toolbar-title>
+        My App v0.0.1
+        </q-toolbar-title>
+        </q-toolbar>
+    </q-header>
+    <q-page-container class="window-width window-height column items-center justify-center">
     <span class="text-h6">My First Vue 3 + Quasar Github Website</span>
-    <q-btn color="primary" label="Click Me"></q-btn>
-    </q-page>
+    <q-btn @click="$q.notify({message: 'Click'})" color="primary" label="Click Me"></q-btn>
+    </q-page-container>
     </q-layout>
     `,
     setup() {
-        
+        return {
+
+        }
     }
 }
